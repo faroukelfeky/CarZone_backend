74041d4938b970211da56cf1af1aee487d1a8f6e
faroukelfeky <faroukdizer2005@gmail.com>
2026-04-22 19:07:49 +0200

    Implement AuthenticationController for auth endpoints

---
+++ CarZone/API/Controllers/AuthenticationController.cs
@@ -0,0 +1,46 @@
+﻿using CarZone.Application.Interfaces;
+using CarZone.Application.Services;
+using CarZone.Infastructure.DTOs.Customer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarZone.API.Controllers
+{
+    [Route("api/v1/auth")]
+    public class AuthenticationController : Controller
+    {
+        private readonly ICustomerService _customerService;
+        public AuthenticationController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+        [HttpPost("sign-up")]
+        public async Task<IActionResult> Signup([FromBody] CustomerCreationDTO customer)
+        {
+            var result = await _customerService.CreateCustomer(customer);
+
+            if (result.status == 0)
+                return BadRequest(new { message = result.msg });
+            else
+                return Ok(result);
+        }
+        [HttpGet("sign-in")]
+        public async Task<IActionResult> Signin([FromQuery] string email, [FromQuery] string password)
+        {
+            var result = await _customerService.Login(email, password);
+            if (result.status == 0)
+                return BadRequest(new { message = result.msg });
+            else
+                return Ok(result);
+        }
+        [HttpPost("sign-up-admin")]
+        public async Task<IActionResult> SignUpAdming([FromBody] CustomerCreationDTO customer)
+        {
+            var result = await _customerService.CreateAdmin(customer);
+
+            if (result.status == 0)
+                return BadRequest(new { message = result.msg });
+            else
+                return Ok(result);
+        }
+    }
+}
